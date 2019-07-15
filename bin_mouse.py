474fd46795b2055a84b296dd06859cd29fac25f0
xalion <xalion67@gmail.com>
2019-07-16 01:14:37 +0300

    add escape symbols

---
+++ bin_mouse.py
@@ -0,0 +1,31 @@
+from numstranslate.alg import dec_to_bin, bin_to_dec
+from random import randint
+from tqdm import tqdm
+
+
+def random_binary_number():
+    """
+    Возвращает случайное двоичное число
+    Это число будет номером пробирки с ядом
+    """
+    return dec_to_bin(randint(1, 1000), bit_depth=10)
+
+
+binary_numbers_vial = []
+print('Вычисляем номера пробирок в двоичном коде')
+for num in tqdm(range(1, 1001)):
+    binary_numbers_vial.append(dec_to_bin(num, bit_depth=10))
+
+print('\nДаём по капле из пробирки мышам.\nЕдиницы - клетки с мышами, которым нужно дать каплю')
+number_vial_poison = random_binary_number()
+
+
+dead_mouse = []
+for i in range(10):
+    if number_vial_poison[i] == '1':
+        dead_mouse.append(i + 1)
+
+print('\nУмерли мыши в клетках №{}'.format(' №'.join([str(number_mouse) for number_mouse in dead_mouse])))
+print('Отмечаем номера погибших мышей единицей справа налево, получается {}'.format(number_vial_poison))
+print('\nПереводим {} в десятичное число.\nПолучается {}, это номер пробирки с ядом'.format(number_vial_poison, bin_to_dec(number_vial_poison)))
+print('Разбор задачи в журнале "Код" - https://thecode.media/binary-mouse/')
