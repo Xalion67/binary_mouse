a8b7c6eceff89e6b3f857149418caa4d7aedf399
Халион <xalion67@gmail.com>
2019-07-18 20:26:16 +0300

    change print info

--- bin_mouse.py
+++ bin_mouse.py
@@ -12,11 +12,11 @@ def random_binary_number():
 
 
 binary_numbers_vial = []
-print('Вычисляем номера пробирок в двоичном коде')
+print('1) Вычисляем номера пробирок в двоичном коде')
 for num in tqdm(range(1, 1001)):
     binary_numbers_vial.append(dec_to_bin(num, bit_depth=10))
 
-print('\nДаём по капле из пробирки мышам.\nЕдиницы - клетки с мышами, которым нужно дать каплю')
+print('\n2) Даём по капле из пробирки мышам.\nЕдиницы - клетки с мышами, которым нужно дать каплю из пробирки')
 number_vial_poison = random_binary_number()
 
 
@@ -25,7 +25,8 @@ for i in range(10):
     if number_vial_poison[i] == '1':
         dead_mouse.append(i + 1)
 
-print('\nУмерли мыши в клетках №{}'.format(' №'.join([str(number_mouse) for number_mouse in dead_mouse])))
+print('\n3) Умерли мыши в клетках №{}'.format(' №'.join([str(number_mouse) for number_mouse in dead_mouse])))
 print('Отмечаем номера погибших мышей единицей справа налево, получается {}'.format(number_vial_poison))
-print('\nПереводим {} в десятичное число.\nПолучается {}, это номер пробирки с ядом'.format(number_vial_poison, bin_to_dec(number_vial_poison)))
+print('\n4) Переводим {} в десятичное число.\nПолучается {}, это номер пробирки с ядом'.format(number_vial_poison,
+                                                                                               bin_to_dec(number_vial_poison)))
 print('Разбор задачи в журнале "Код" - https://thecode.media/binary-mouse/')
